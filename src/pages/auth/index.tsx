1d194bb21b1beca38f4f0f5b4d564e9557189d9a
iadokonat <iadokonat0106@gmail.com>
2023-01-14 23:08:04 +0900

    Add Auth page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,18 +1,21 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
 
+import { AuthForm } from '@/components/molcules/AuthForm'
 import { Layout } from '@/components/templates/Layout'
 
-const Home: NextPage = () => {
+const AuthPage: NextPage = () => {
   return (
     <>
       <Head>
         <title>ToDo List</title>
       </Head>
 
-      <Layout>Top Page</Layout>
+      <Layout>
+        <AuthForm />
+      </Layout>
     </>
   )
 }
 
-export default Home
+export default AuthPage
